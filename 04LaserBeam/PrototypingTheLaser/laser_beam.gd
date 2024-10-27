aca64f2b368e280b3ce759d7579312d237f31f64
BlurryGames <julian.lb.games@gmail.com>
2024-10-27 20:09:51 +0100

    Section4: Laser Beam / Lesson5: Emitting particles dynamically

--- 04LaserBeam/PrototypingTheLaser/laser_beam.gd
+++ 04LaserBeam/PrototypingTheLaser/laser_beam.gd
@@ -11,6 +11,10 @@ var is_casting: bool = false:
 
 @onready var fill: Line2D = %FillLine2D
 
+@onready var casting_particles: GPUParticles2D = %CastingParticles2D
+@onready var collision_particles: GPUParticles2D = %CollisionParticles2D
+@onready var beam_particles: GPUParticles2D = %BeamParticles2D
+
 @onready var line_with: float = fill.get_width()
 
 func _ready() -> void:
@@ -37,14 +41,23 @@ func set_is_casting(cast: bool) -> void:
 		dissapear()
 	
 	set_physics_process(is_casting)
+	casting_particles.set_emitting(is_casting)
+	beam_particles.set_emitting(is_casting)
 
 func cast_beam() -> void:
 	var cast_point: Vector2 = get_target_position()
 	force_raycast_update()
+	collision_particles.set_emitting(is_colliding())
 	if is_colliding():
 		cast_point = to_local(get_collision_point())
+		collision_particles.set_global_rotation(get_collision_normal().angle())
+		collision_particles.set_position(cast_point)
 	
 	fill.points[1] = cast_point
+	beam_particles.set_position(cast_point * 0.5)
+	var particle_material: ParticleProcessMaterial = beam_particles.process_material
+	if particle_material and particle_material.get_emission_shape() == ParticleProcessMaterial.EMISSION_SHAPE_BOX:
+		particle_material.emission_box_extents.x = cast_point.length() * 0.5
 
 func apper() -> void:
 	if tween and tween.is_running():
@@ -59,3 +72,4 @@ func dissapear() -> void:
 	
 	tween = create_tween()
 	tween.tween_property(fill, ^"width", 0.0, growth_time)
+	collision_particles.set_emitting(false)
