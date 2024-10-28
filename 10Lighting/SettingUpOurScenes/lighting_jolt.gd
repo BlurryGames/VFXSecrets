a19d83d6f7cbcfb8f4d44c83c320e941ddc617e0
BlurryGames <julian.lb.games@gmail.com>
2024-10-28 18:14:43 +0100

    Section10: Lighting / Lesson3: The lighting jolt script

--- 10Lighting/SettingUpOurScenes/lighting_jolt.gd
+++ 10Lighting/SettingUpOurScenes/lighting_jolt.gd
@@ -1,6 +1,27 @@
 class_name LightingJolt extends Line2D
 
+@export_range(0.5, 3.0, 0.01) var spread_angle: float = PI / 4.0
+
+@export_range(1, 36, 1) var segments: int = 12
+
 @onready var sparks: GPUParticles2D = %Sparks
 
+func _ready() -> void:
+	set_as_top_level(true)
+
 func create(start: Vector2, end: Vector2) -> void:
-	pass
+	var _points: Array[Vector2] = []
+	var current: Vector2 = start
+	var segment_lenght: float = start.distance_to(end) / segments
+	
+	_points.append(start)
+	for segment in range(segments):
+		var _rotation: float = randf_range(-spread_angle * 0.5, spread_angle * 0.5)
+		var _new: Vector2 = current + (current.direction_to(end) * segment_lenght).rotated(_rotation)
+		_points.append(_new)
+		
+		current = _new
+	
+	_points.append(end)
+	set_points(_points)
+	sparks.set_global_position(end)
